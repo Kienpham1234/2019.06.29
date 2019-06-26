cbd1da8f39f98f0e0ef06e4110bed816d3d40074
Kien Pham <747782@student.tor.loc>
2019-06-26 15:29:42 -0400

    second commit

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-function waittingFn(timeInMs){
+function waitingFn(timeInMs){
     futureTime = Date.now() + timeInMs;
     while (futureTime>Date.now()){
      //waiting...   
     }
 }
 
-
-waitting(3000);
+debugger;
+waitingFn(3000);
 console.log("Function called has just ended!");
